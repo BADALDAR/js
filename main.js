8a07c0a8de43a213cba504684519b9d836a726ed
Алдар Бадмаев <94624330+BADALDAR@users.noreply.github.com>
2023-06-24 00:57:36 +1000

    new version

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(2 + 3)
+// console.log('other')
+const students = [
+	{ name: 'Вероника', age: 34, sex: 'Ж', eq: 'snowboard', level: 3 },
+	{ name: 'Елена', age: 40, sex: 'Ж', level: 2, eq: 'ski', level: 2 },
+	{ name: 'Виталий', age: 42, sex: 'М', level: 3, eq: 'snowboard', level: 4 },
+]
+// FOR EACH
+// students.forEach(function (test) {
+// 	console.log(test)
+// })
+students.forEach(st => console.log(st.name + st.sex))
